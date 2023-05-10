f9cf687bba54bd5157052525d27174dc7b2342fb
parthpandyappp <parthpandyappp@gmail.com>
2023-05-11 03:01:01 +0530

    fix: border color in production

--- src/components/TodaySchedule.tsx
+++ src/components/TodaySchedule.tsx
@@ -3,7 +3,7 @@ const TodaySchedule = (props: any) => {
     s: { color, title, time, at },
   } = props;
   return (
-    <span className={`border-l-4 border-[${color}] px-3.5`}>
+    <span className={`border-l-4 ${color} px-3.5`}>
       <p className="text-[#666666] font-bold font-lato">{title}</p>
       <p className="text-sm text-[#999999] font-lato">{time}</p>
       <p className="text-sm text-[#999999] font-lato">{at}</p>
